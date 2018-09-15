7b53a7ed70ed62ebf40de8e5090b6e2bc846aa2f
yo-ch <yoson.chiu@hotmail.com>
2018-09-15 16:05:44 -0400

    Update to use Anilist API because MAL API is never coming back up.

--- anilist.js
+++ anilist.js
@@ -1,100 +1,90 @@
-const userPass = 'base64 encoded user:pass';
-if (location.href.search(/\/anime\/|\/manga\//) > 0)
+let currentPage = window.location.href;
+if (window.location.href.search(/\/anime\/|\/manga\//) > 0) {
     retrieveMALURL();
-setTimeout(pageObserver, 2000);
-
-/*
-Add a page observer to check when the page has changed (i.e; A new anime/manga has been loaded.)
-*/
-function pageObserver() {
-    let handler;
-    let observer = new MutationObserver(function (mutations) {
-        try {
-            if (mutations[0].addedNodes[0].id == 'view' && (location.href.search(
-                    /\/anime\/|\/manga\//) > 0)) {
-                retrieveMALURL();
-            }
-        } catch (err) {}
-    });
-    let config = {
-        attributes: true,
-        childList: true,
-        characterData: true
-    };
-    observer.observe(document.body, config);
 }
 
+/**
+ * Check for page change every 50ms.
+ */
+setInterval(() => {
+    if (currentPage != window.location.href &&
+        window.location.href.search(/\/anime\/|\/manga\//) > 0) {
+        currentPage = window.location.href;
+        retrieveMALURL();
+    }
+}, 100);
 /*
 Retrieve the corresponding MAL URL of the anime/manga.
 */
 function retrieveMALURL() {
-    //Retrieve title. If empty/null, try again in 100ms.
-    let title = '';
-    let tags = document.getElementsByTagName('h1');
-    if (tags.length == 0 || !tags[0] || tags[0].innerText == '') {
-        return setTimeout(retrieveMALURL, 100);
-    } else {
-        title = tags[0].innerText.split(/\s+/).join('+');
-    }
-
-    //Check whether page is an anime or a manga.
-    let type = location.href.search('anime') >= 0 ?
+    //Get page type (anime/manga) and Anilist id.
+    const type = location.href.indexOf('anime') >= 0 ?
         'anime' : 'manga';
+    const id = parseInt(location.href.match(/\d+/)[0]);
 
-    //Make MAL API request.
-    let getMAL = new XMLHttpRequest();
-    getMAL.open('GET', `https://myanimelist.net/api/${type}/search.xml?q=${title}`);
-    getMAL.setRequestHeader('Authorization', `Basic ${userPass}`);
-    getMAL.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-
-            //Parse ID to get MAL link.
-            let parser = new DOMParser();
-            let xmlDoc = parser.parseFromString(this.responseText, 'text/html');
-
-            let typeArray = xmlDoc.getElementsByTagName('type');
-            let desiredIndex = null; //The index of the desired response (TV type), if matched.
-            for (let i = 0; i < typeArray.length; i++) {
-                if (typeArray[i].innerText == 'TV' && type == 'anime' || typeArray[i].innerText ==
-                    'Manga' && type == 'manga') {
-                    desiredIndex = i;
-                    break;
-                }
+    //Construct query.
+    const query =
+        `
+        query ($id: Int) {
+            Media (id: $id) {
+               idMal
             }
-
-            let id = xmlDoc.getElementsByTagName('id')[desiredIndex != null ?
-                desiredIndex : 0].innerText; //Use index if found.
-            let score = xmlDoc.getElementsByTagName('score')[0].innerText;
-            injectMALButton(id, type, score);
         }
-    }
-    getMAL.send();
+        `;
+    const variables = { id };
+
+    //Call Anilist API to get MAL id of anime/manga.
+    fetch(`https://graphql.anilist.co/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({ query, variables })
+        })
+        .then(resp => resp.json())
+        .then(resp => {
+            const malId = resp.data.Media.idMal;
+            injectMALButton(malId, type);
+        })
+        .catch(error => {
+            console.log(error);
+        })
 }
 
-/*
-Injects a MAL link to the corresponding anime/manga on MAL.
-*/
-function injectMALButton(id, type, score) {
+/**
+ * Injects a link on the Anilist page to the corresponding media on MAL.
+ * @param id {Number} the mal id of the anime/manga.
+ * @param type {String} the type of media.
+ */
+function injectMALButton(id, type) {
     let malLink = `https://myanimelist.net/${type}/${id}`;
 
-    let nav = document.getElementsByClassName('series__title')[0];
+    let nav = document.getElementsByClassName('actions')[0];
 
     if (!nav) {
-        return setTimeout(injectMALButton(id, type), 50);
+        return setTimeout(injectMALButton(id, type), 100);
     }
 
+    //Clear old button.
+    let malLinks = document.getElementsByClassName('malLink');
+    if (malLinks.length) {
+        nav.removeChild(malLinks[0]);
+    }
+
+    //Set actions column to 3 and repad buttons.
+    nav.style['grid-template-columns'] = 'auto 35px 35px';
+    nav.children[0].children[0].style['padding-left'] = '0px';
+    nav.children[0].children[0].style['font-size'] = '.95em';
+
+    //Create new button.
     let button = document.createElement('img');
     button.src = chrome.extension.getURL('images/malIcon.png');
     button.className = 'malImage';
     button.alt = 'MAL';
 
-    let scoreText = document.createElement('div');
-    scoreText.appendChild(document.createTextNode(score));
-    scoreText.className = 'scoreText';
-
     let link = document.createElement('a');
     link.appendChild(button);
-    link.appendChild(scoreText);
 
     link.href = malLink;
     link.target = '_blank';
